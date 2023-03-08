333af1ee59031d9df210a60c678d16f5339eae08
Bhagyodaya-Varshney <126950178+Bhagyodaya-Varshney@users.noreply.github.com>
2023-03-08 19:00:01 +0530

    Add files via upload

---
+++ first_and_last_letter.java
@@ -0,0 +1,11 @@
+public class first_and_last_letter {
+    public static void main(String[] args) {
+        char arr[]={'V','I','K','A','S',' ','C','A','U'};
+        System.out.print(arr[0]);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==' '){
+                System.out.println(charAt);
+            }
+        }
+    }
+}

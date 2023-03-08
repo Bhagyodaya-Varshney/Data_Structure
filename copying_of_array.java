e17c2ccb6f5d16b1ceb129e7f86639ead8c93d26
Bhagyodaya-Varshney <126950178+Bhagyodaya-Varshney@users.noreply.github.com>
2023-03-08 19:00:42 +0530

    Add files via upload

---
+++ copying_of_array.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class copying_of_array {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int arr1[]=new int[arr.length];
+        for(int i=arr.length-1; i>=0;i--){
+            arr1[i]=arr[i];
+            System.out.print("["+arr1[i]+"]");
+        }
+    }
+}
